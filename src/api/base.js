9ec813fcdddecd77386bb848a5d51074fd54923d
robinnayak <robinnayak86@gmail.com>
2024-09-19 11:06:51 +0545

    updated url

--- src/api/base.js
+++ src/api/base.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://disha.up.railway.app/api"
-export const BASE_URL = "http://10.0.2.2:8000/api"
+export const BASE_URL = "https://disha.up.railway.app/api"
+// export const BASE_URL = "http://10.0.2.2:8000/api"
 export const MEDIA_URL = "https://res.cloudinary.com/dckab8f6g/"
 // https://res.cloudinary.com/dckab8f6g/
 
